cb3ee3230faa194d4bc604bcd9ccc073ea44b7a1
guthi <guthi@Saibabu>
2025-04-01 09:42:17 +0530

    Push code

--- CollabFrameWork/src/test/java/contactTest/ToCreateContact.java
+++ CollabFrameWork/src/test/java/contactTest/ToCreateContact.java
@@ -4,7 +4,9 @@ public class ToCreateContact {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     System.out.println("my testcase is passed");
+     System.out.println("======================");
+     System.out.println("my testcase is excuited successfully  ");
 	}
 
 }
